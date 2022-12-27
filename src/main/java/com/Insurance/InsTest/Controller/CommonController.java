ba85c9c07646ee5a9e1d9c6c45d46c33eff6f79d
varun <23652varun@gmail.com>
2022-12-27 19:42:37 +0530

    logger phase

--- src/main/java/com/Insurance/InsTest/Controller/CommonController.java
+++ src/main/java/com/Insurance/InsTest/Controller/CommonController.java
@@ -1,17 +1,17 @@
 package com.Insurance.InsTest.Controller;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.Insurance.InsTest.Exception.GlobalExceptionHandler;
 import com.Insurance.InsTest.Exception.ResourceNotFoundException;
 import com.Insurance.InsTest.Model.AccountType;
 import com.Insurance.InsTest.Model.Bank;
@@ -20,12 +20,7 @@ import com.Insurance.InsTest.Model.DocumentType;
 import com.Insurance.InsTest.Model.Master;
 import com.Insurance.InsTest.Model.PlanType;
 import com.Insurance.InsTest.Model.States;
-import com.Insurance.InsTest.Repository.AccountTypeRepository;
-import com.Insurance.InsTest.Repository.BankRepository;
 import com.Insurance.InsTest.Repository.CountryRepository;
-import com.Insurance.InsTest.Repository.DocumentTypeRepository;
-import com.Insurance.InsTest.Repository.PlanTypeRepository;
-import com.Insurance.InsTest.Repository.StatesRepository;
 import com.Insurance.InsTest.Service.AccountTypeService;
 import com.Insurance.InsTest.Service.BankService;
 import com.Insurance.InsTest.Service.CountryService;
@@ -33,43 +28,50 @@ import com.Insurance.InsTest.Service.DocumentTypeService;
 import com.Insurance.InsTest.Service.PlanTypeService;
 import com.Insurance.InsTest.Service.StatesService;
 
+
+
 @RestController
 @RequestMapping("/common")    
 public class CommonController {
 
 	//objects
-	
-	@Autowired
-	 CountryRepository countryRepository;
-	
-	@Autowired
-	 StatesService stateservice;
-	
-	@Autowired
-	 CountryService countryService;
 
 	@Autowired
-	 DocumentTypeService documentTypeService;
-	
+	CountryRepository countryRepository;
+
 	@Autowired
-	 AccountTypeService accountTypeService;
-	
+	StatesService stateservice;
+
 	@Autowired
-	 BankService bankService;
-	
+	CountryService countryService;
+
 	@Autowired
-	 PlanTypeService typeService; 
+	DocumentTypeService documentTypeService;
+
+	@Autowired
+	AccountTypeService accountTypeService;
+
+	@Autowired
+	BankService bankService;
+
+	@Autowired
+	PlanTypeService typeService; 
+
 	
+
+	//logger object
 	
-	
-	
+	Logger logger= LoggerFactory.getLogger(CommonController.class);
+
 	//methods to get all details of entity
-	
 	@GetMapping("/allcountry")  //ALL
 	public List<Country> getAllCountry()
 	{	
 		try
 		{
+			logger.info("[getAllCountry] to get all country list ");
+			logger.debug("debug msg");
+			
 			long totalcount=countryRepository.count();
 			return countryRepository.findAll();
 		}
@@ -85,59 +87,61 @@ public class CommonController {
 	{
 		return stateservice.findAllStates();
 	}
-	
+
 	@GetMapping("/alldocument")  //ALL
 	public List<DocumentType> getAllDocument()
 	{
 		return documentTypeService.findAllDocumentType();
 	}
-	
+
 	@GetMapping("/allaccountype")  //ALL
 	public List<AccountType> getAllAccountType()
 	{
 		return accountTypeService.getAllAccountType();
 	}
-		
+
 	@GetMapping("/allbank")  //ALL
 	public List<Bank> getAllBank()
 	{
 		return bankService.getAllBank();
 	}	
-	
-	
+
+
 	@GetMapping("/allplantype")  //ALL
 	public List<PlanType> getAllPlanType()
 	{
 		return typeService.getAllPlanType();
 	}
-		
-	
-	
+
+
+
 	//method to get alldetails of the present entities
-	
+
 	@GetMapping("/getmasterdata")
 	public Master getAllEntity() throws ResourceNotFoundException
 	{
 		Master master=new Master();
-	
+
 		master.country=getAllCountry();
-		
+
 		master.state=getAllStates();
-		
+
 		master.documenttype=getAllDocument();
-		
+
 		master.accounttypes=getAllAccountType();
-		
+
 		master.bank=getAllBank();
-		
+
 		master.plantype=getAllPlanType();
+
+		
 		
 		return master;
-				
+
 	}
-	
+
 	//method to get by id
-	
+
 	@GetMapping("/countryid/{id}")//byid
 	public ResponseEntity<Country> getCountryById(@PathVariable(value="id") Long countryId )
 			throws ResourceNotFoundException
@@ -157,7 +161,7 @@ public class CommonController {
 
 		return ResponseEntity.ok().body(states);
 	}
-	
+
 	@GetMapping("/bankid/{id}")//byid
 	public ResponseEntity<Bank> getBankById(@PathVariable(value="id") Long bankId )
 			throws ResourceNotFoundException
@@ -167,7 +171,7 @@ public class CommonController {
 
 		return ResponseEntity.ok().body(bank);
 	}
-	
+
 	@GetMapping("/documenttypeid/{id}")//byid
 	public ResponseEntity<DocumentType> getDocumentById(@PathVariable(value="id") Long DocumentTypeId )
 			throws ResourceNotFoundException
@@ -177,7 +181,7 @@ public class CommonController {
 
 		return ResponseEntity.ok().body(documentType);
 	}
-	
+
 	@GetMapping("/accounttypeid/{id}")//byid
 	public ResponseEntity<AccountType> getAccountTypeById(@PathVariable(value="id") Long accountTypeId )
 			throws ResourceNotFoundException
